75fd3c879654d2440ceadc8dea6967eac7bc6f53
Roger Martins <roger.8b@gmail.com>
2019-01-23 23:27:55 -0200

    Movendo o Card (Não esta funcionando)

--- lib/main.dart
+++ lib/main.dart
@@ -1,318 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:fluttery/layout.dart';
+import 'package:flutter_tinder_cards/ui/card.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Tinder Cards',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
         primaryColorBrightness: Brightness.light,
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-Widget _buildCardStack() {
-  return AnchoredOverlay(
-    showOverlay: true,
-    child: Center(),
-    overlayBuilder: (BuildContext context, Rect anchorBounds, Offset anchor) {
-      return CenterAbout(
-        position: anchor,
-        child: Container(
-          width: anchorBounds.width,
-          height: anchorBounds.height,
-          padding: const EdgeInsets.all(16.0),
-          child: ProfileCard(),
-        ),
-      );
-    },
-  );
-}
-
-class ProfileCard extends StatefulWidget {
-  @override
-  _ProfileCardState createState() => _ProfileCardState();
-}
-
-class _ProfileCardState extends State<ProfileCard> {
-  Widget _buildBackground() {
-
-    return PhotoBrowser(
-      photoAssetPaths: [
-        'assets/images/001.jpg',
-        'assets/images/002.jpg',
-        'assets/images/003.jpg',
-        'assets/images/004.jpg'
-      ],
-      visiblePhotoIndex: 0,
-    );
-  }
-
-  Widget _buildProfileSynopsis() {
-    return new Positioned(
-      left: 0.0,
-      right: 0.0,
-      bottom: 0.0,
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.transparent, Colors.black.withOpacity(0.8)],
-          ),
-        ),
-        padding: const EdgeInsets.all(24.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'Titulo da imagem',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                  ),
-                  Text(
-                    'Descrição da imagem',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Icon(
-              Icons.info,
-              color: Colors.white,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0x11000000),
-              blurRadius: 5.0,
-              spreadRadius: 2.0,
-            )
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10.0),
-          child: Material(
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[_buildBackground(), _buildProfileSynopsis()],
-            ),
-          ),
-        ));
-  }
-}
-
-class PhotoBrowser extends StatefulWidget {
-  final List<String> photoAssetPaths;
-  final int visiblePhotoIndex;
-
-  PhotoBrowser({
-    key,
-    this.photoAssetPaths,
-    this.visiblePhotoIndex,
-  });
-
-  @override
-  _PhotoBrowserState createState() => _PhotoBrowserState();
-}
-
-class _PhotoBrowserState extends State<PhotoBrowser> {
-  int visiblePhotoIndex;
-
-  @override
-  void initState() {
-    super.initState();
-    visiblePhotoIndex = widget.visiblePhotoIndex;
-  }
-
-  @override
-  void didUpdateWidget(PhotoBrowser oldWidget) {
-    if (widget.visiblePhotoIndex != oldWidget.visiblePhotoIndex) {
-      Future.delayed(Duration.zero, () =>
-      setState(() {
-        visiblePhotoIndex = widget.visiblePhotoIndex;
-      }));
-    }
-  }
-
-  void _prevImage() {
-    Future.delayed(Duration.zero, () =>
-    setState(() {
-      visiblePhotoIndex = visiblePhotoIndex > 0 ? visiblePhotoIndex - 1 : 0;
-    }));
-  }
-
-  void _nextImage() {
-    Future.delayed(Duration.zero, () =>
-    setState(() {
-      visiblePhotoIndex = visiblePhotoIndex < widget.photoAssetPaths.length - 1
-          ? visiblePhotoIndex + 1
-          : visiblePhotoIndex;
-    }));
-  }
-
-  Widget _buildPhotoControls() {
-    return new Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        new GestureDetector(
-          onTap: _prevImage,
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            heightFactor: 1.0,
-            alignment: Alignment.topLeft,
-            child: Container(
-              color: Colors.transparent,
-            ),
-          ),
-        ),
-        GestureDetector(
-          onTap: _nextImage,
-          child: FractionallySizedBox(
-            widthFactor: 0.5,
-            heightFactor: 1.0,
-            alignment: Alignment.topRight,
-            child: Container(
-              color: Colors.transparent,
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Image.asset(
-          widget.photoAssetPaths[visiblePhotoIndex],
-          fit: BoxFit.cover,
-        ),
-        Positioned(
-          top: 0.0,
-          left: 0.0,
-          right: 0.0,
-          child: SelectedPhotoIndicator(
-            photoCount: widget.photoAssetPaths.length,
-            visiblePhotoIndex: visiblePhotoIndex,
-          ),
-        ),
-
-        _buildPhotoControls()
-      ],
-    );
-  }
-}
-
-class SelectedPhotoIndicator extends StatelessWidget {
-  final int photoCount;
-  final int visiblePhotoIndex;
-
-  SelectedPhotoIndicator({
-    this.photoCount,
-    this.visiblePhotoIndex,
-  });
-
-  Widget _buildInactiveIndicator() {
-    return Expanded(
-      child: Padding(
-        padding: EdgeInsets.only(left: 2.0, right: 2.0),
-        child: Container(
-          height: 3.0,
-          decoration: new BoxDecoration(
-            color: Colors.black.withOpacity(0.2),
-            borderRadius: BorderRadius.circular(2.5),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildActiveIndicator() {
-    return Expanded(
-      child: Padding(
-        padding: EdgeInsets.only(left: 2.0, right: 2.0),
-        child: Container(
-          height: 3.0,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(2.5),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0x22000000),
-                  spreadRadius: 0.0,
-                  blurRadius: 2.0,
-                  offset: const Offset(0.0, 1.0),
-                )
-              ]),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildIndicators() {
-    List<Widget> indicators = [];
-    for (int i = 0; i < photoCount; ++i) {
-      indicators.add(
-        i == visiblePhotoIndex
-            ? _buildActiveIndicator()
-            : _buildInactiveIndicator(),
-      );
-    }
-    return indicators;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Row(
-        children: _buildIndicators(),
-      ),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   Widget _buildAppBar() {
-    return AppBar(
+    return new AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0.0,
-      leading: IconButton(
-        icon: Icon(Icons.person),
+      leading: new IconButton(
+        icon: new Icon(
+          Icons.person,
+          color: Colors.grey,
+          size: 40.0,
+        ),
         onPressed: () {
           // TODO:
         },
@@ -322,58 +42,62 @@ class _MyHomePageState extends State<MyHomePage> {
         colors: Colors.red,
       ),
       actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.chat_bubble),
+        new IconButton(
+          icon: new Icon(
+            Icons.chat_bubble,
+            color: Colors.grey,
+            size: 40.0,
+          ),
           onPressed: () {
             // TODO:
           },
-        ),
+        )
       ],
     );
   }
 
-  Widget _buildBottonBar() {
+  Widget _buildBottomBar() {
     return BottomAppBar(
       color: Colors.transparent,
       elevation: 0.0,
       child: new Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
+        padding: const EdgeInsets.all(16.0),
+        child: new Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            RoundIconButton.small(
+            new RoundIconButton.small(
               icon: Icons.refresh,
               iconColor: Colors.orange,
               onPressed: () {
-                //TODO:
+                // TODO:
               },
             ),
-            RoundIconButton.large(
+            new RoundIconButton.large(
               icon: Icons.clear,
               iconColor: Colors.red,
               onPressed: () {
-                //TODO:
+                // TODO:
               },
             ),
-            RoundIconButton.small(
+            new RoundIconButton.small(
               icon: Icons.star,
               iconColor: Colors.blue,
               onPressed: () {
-                //TODO:
+                // TODO:
               },
             ),
-            RoundIconButton.large(
+            new RoundIconButton.large(
               icon: Icons.favorite,
               iconColor: Colors.green,
               onPressed: () {
-                //TODO:
+                // TODO:
               },
             ),
-            RoundIconButton.small(
+            new RoundIconButton.small(
               icon: Icons.lock,
               iconColor: Colors.purple,
               onPressed: () {
-                //TODO:
+                // TODO:
               },
             ),
           ],
@@ -384,10 +108,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       appBar: _buildAppBar(),
-      body: _buildCardStack(),
-      bottomNavigationBar: _buildBottonBar(),
+      body: new DraggableCard(),
+      bottomNavigationBar: _buildBottomBar(),
     );
   }
 }
@@ -398,30 +122,44 @@ class RoundIconButton extends StatelessWidget {
   final double size;
   final VoidCallback onPressed;
 
-  RoundIconButton.large({this.icon, this.iconColor, this.onPressed})
-      : size = 60.0;
+  RoundIconButton.large({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 60.0;
 
-  RoundIconButton.small({this.icon, this.iconColor, this.onPressed})
-      : size = 50.0;
+  RoundIconButton.small({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 50.0;
 
-  RoundIconButton({this.icon, this.iconColor, this.size, this.onPressed});
+  RoundIconButton({
+    this.icon,
+    this.iconColor,
+    this.size,
+    this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return new Container(
       width: size,
       height: size,
-      decoration: BoxDecoration(
+      decoration: new BoxDecoration(
         shape: BoxShape.circle,
         color: Colors.white,
         boxShadow: [
-          new BoxShadow(color: const Color(0x11000000), blurRadius: 10.0)
+          new BoxShadow(
+            color: const Color(0x11000000),
+            blurRadius: 10.0,
+          ),
         ],
       ),
-      child: RawMaterialButton(
-        shape: CircleBorder(),
+      child: new RawMaterialButton(
+        shape: new CircleBorder(),
         elevation: 0.0,
-        child: Icon(
+        child: new Icon(
           icon,
           color: iconColor,
         ),
